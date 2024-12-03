e67ac10f9af983dbcc3fa864f2a04453d13913d3
ElNico56 <meeqnico@gmail.com>
2024-12-03 16:52:35 -0300

    WiWi

--- 2024.3.lua
+++ 2024.3.lua
@@ -1,21 +1,20 @@
 -- AoC 2024 day 3 --
 
 local input = assert(io.open"2024.3.in"):read"*a"
-input = input..string.rep(" ", 12)
 
 local p1, p2 = 0, 0
 local enabled = true
-for i = 1, #input - 12 do
-	local str = input:sub(i, i + 12)
-	if str:match"^don't%(%)" then
-		enabled = false
-	elseif str:match"^do%(%)" then
+
+for command in input:gmatch"[%w']+%(%d*,?%d*%)" do
+	if command:match"do%(%)" then
 		enabled = true
-	elseif str:match"^mul%(%d+,%d+%)" then
-		local a, b = str:match"^mul%((%d+),(%d+)%)"
+	elseif command:match"don't%(%)" then
+		enabled = false
+	elseif command:match"mul%(%d+,%d+%)" then
+		local a, b = command:match"mul%((%d+),(%d+)%)"
 		a, b = tonumber(a), tonumber(b)
 		p1 = p1 + a * b
-		if enabled then p2 = p2 + a * b end
+		p2 = p2 + (enabled and a * b or 0)
 	end
 end
 

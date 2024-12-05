b3d9b6abb64be7adbbf9dcf7a861f9a2ffe8bb06
ElNico56 <meeqnico@gmail.com>
2024-12-05 00:29:06 -0300

    butter

--- 2015.4.lua
+++ 2015.4.lua
@@ -6,18 +6,14 @@ local input = assert(io.open"2015.4.in"):read"*a"
 local p1 = 0
 while true do
 	local hash = md5.sumhexa(input..p1)
-	if hash:sub(1, 5) == "00000" then
-		break
-	end
+	if hash:sub(1, 5) == "00000" then break end
 	p1 = p1 + 1
 end
 
 local p2 = 0
 while true do
 	local hash = md5.sumhexa(input..p2)
-	if hash:sub(1, 6) == "000000" then
-		break
-	end
+	if hash:sub(1, 6) == "000000" then break end
 	p2 = p2 + 1
 end
 
